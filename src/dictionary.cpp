a6cf3c1e2d2ddc14d8f77e739d0a71ed89533b30
ChickenGolem <98434953+ChickenGolem@users.noreply.github.com>
2026-03-23 10:53:38 -0400

    added functionality to dictionary.cpp

--- src/dictionary.cpp
+++ src/dictionary.cpp
@@ -8,30 +8,70 @@
 
 #include "dictionary.h"
 #include "d_except.h"
+#include <iostream>
+#include <fstream>
 using namespace std;
 
 
 Dictionary::Dictionary(string dictName)
 // given a dictionary file, creates Dictionary object
 {
+    string DictLine;
     // open and read file
+    ifstream DictFile("dictionary.txt");
     // read words into vec
+    while (getline (DictFile, DictLine)) {
+        words.push_back(DictLine);
+    }
 }
 
 void Dictionary::sort()
 // uses selection sort to organize dictionary word vector
 {
-    // implement selection sort algorithm
+    // iterates through vector
+    for(int i = 0; i < words.size(); ++i) {
+        int current_min = i;
+        // iterates through unsorted portion of vector, keeping track of index with the lowest value
+        for(int j = i + 1; j < words.size(); ++j){
+            if(words[j] < words[current_min]){
+                current_min = j;
+            }
+        }
+        // swaps 1st unsorted index with the lowest unsorted value
+        swap(words[i], words[current_min]);
+    }
 }
 
 int Dictionary::wordLookup(string word) const
 // given a word, returns index of word in dictionary if it exists, else -1
 {
+    int lowest = 0;
+    int highest = words.size() - 1;
 
+    while(lowest <= highest){
+        int middle = (lowest + highest) / 2;
+        // if word is found this evaulates as true and returns index of word
+        if(words[middle] == word){
+            return middle;
+        }
+        // if word is greater than middle word, changes the lowest possible index to midd + 1
+        else if(words[middle] < word){
+            lowest = middle + 1;
+        }
+        // only get here if words[middle] > word, so now highest possible value is middle - 1
+        else{
+            highest = middle - 1;
+        }
+    }
+    // returns -1 if not found
     return -1;
 }
 
 ostream& operator<<(ostream& ostr, const Dictionary& dict)
 {
     // print all words in dictionary
+    for(int i = 0; i < dict.words.size(); i++){
+        ostr << dict.words[i] << '\n';
+    }
+    return ostr;
 }
