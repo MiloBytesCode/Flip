80c70414f10cf9d4bd2f3215da77064f8c2f60d6
1214sean <seanhuang922922@gmail.com>
2026-03-31 18:25:51 -0400

    Update heap.h

--- src/heap.h
+++ src/heap.h
@@ -5,7 +5,7 @@
 //
 // The header for the Grid Class
 /* ========================================================================= */
-
+#pragma once
 #include <iostream>
 #include <string>
 #include <vector>
@@ -17,11 +17,16 @@ class Heap
 {
     public:
 
+        int parent(int) const;
+        int left(int) const;
+        int right(int) const;
+        T getItem(int) const;
+    /*
         T parent(int); //returns value of parent of a node
         T left(int); //returns value of left child of node
         T right(int); // returns value of right child of node
         T getItem(int); // returns node value
-
+    */
         void initializeMaxHeap(vector<T>); //set stored to vector and sort
         void maxHeapify(int); //max heapify a single node
         void buildMaxHeap(); //sort stored vector
@@ -30,6 +35,7 @@ class Heap
 
     private:
         vector<T> stored; //heap
+        int heapSize;
 
 };
 
